16547e2fd441efa9ce168bf1f98b3b3b223695e7
trickypr <trickypr@icloud.com>
2022-11-16 20:39:16 +1100

    🐛 Rewrite the way we handle sizes to make it more correct

--- src/main/java/com/trickypr/bookban/events/PlayerEvent.java
+++ src/main/java/com/trickypr/bookban/events/PlayerEvent.java
@@ -1,11 +1,11 @@
 package com.trickypr.bookban.events;
 
-import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -14,14 +14,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.bukkit.inventory.meta.BlockStateMeta;
 
 import com.trickypr.bookban.ConfigDependant;
 
 public class PlayerEvent implements Listener, ConfigDependant {
     FileConfiguration config;
 
-    int maxInventorySize = 36000;
+    int maxInventorySize = 27448;
+    int maxItemSize = 9269;
+    int maxBookSize = 8000;
+
+    boolean disabled = false;
+    boolean debug = false;
 
     public PlayerEvent(FileConfiguration config) {
         setConfig(config);
@@ -31,72 +36,99 @@ public class PlayerEvent implements Listener, ConfigDependant {
         this.config = config;
 
         maxInventorySize = config.getInt("limit.inventory");
+        maxItemSize = config.getInt("limit.item");
+        maxBookSize = config.getInt("limit.book");
+
+        disabled = config.getBoolean("disabled");
+        debug = config.getBoolean("debug");
+    }
+
+    private int getItemSize(ItemStack stack) {
+        if (stack == null)
+            return 0;
+
+        int size = 0;
+
+        BlockStateMeta meta = null;
+
+        if (stack.hasItemMeta())
+            if (stack.getItemMeta() instanceof BlockStateMeta)
+                meta = (BlockStateMeta) stack.getItemMeta();
+
+        // Item metadata is used for the contents of shulkerboxes, so we
+        // need to find its contents and serialize it for the shulker box
+        // size
+        if (meta != null && meta.getBlockState() instanceof ShulkerBox) {
+            ShulkerBox shulker = (ShulkerBox) meta.getBlockState();
+            size += inventorySize(shulker.getInventory().getContents());
+        }
+
+        size += stack.serializeAsBytes().length;
+
+        return size;
     }
 
     // Return inventory size in bytes
     private int inventorySize(ItemStack[] contents) {
-        try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            BukkitObjectOutputStream data = new BukkitObjectOutputStream(os);
-            data.writeObject(contents);
-            data.close();
-            os.close();
-            return os.toByteArray().length;
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return 5000;
+        int size = 0;
+        for (ItemStack stack : contents) {
+            size += getItemSize(stack);
         }
+        return size;
     }
 
     @EventHandler
     public void onPickup(EntityPickupItemEvent e) {
-        if (!(e.getEntity() instanceof Player)) {
-            // We do not care about non-player entities for the moment
+        if (disabled)
+            return;
+
+        // We do not care about non-player entities for the moment
+        if (!(e.getEntity() instanceof Player))
+            return;
+
+        LivingEntity entity = e.getEntity();
+        Player player = (Player) entity;
+
+        ItemStack item = e.getItem().getItemStack();
+
+        int invSize = inventorySize(player.getInventory().getContents());
+        int itemSize = getItemSize(item);
+
+        if (debug) {
+            Bukkit.getLogger().info("Player: " + player.getName() + ", Item: " + itemSize
+                    + " bytes, Inventory: " + invSize + " bytes, Total: " + (invSize + itemSize) + " bytes.");
+        }
+
+        // Check if the item is larger than any single item is allowed to be
+        if (itemSize > maxItemSize) {
+            e.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "You cannot pick up this item, it is to large (" + itemSize + " > "
+                    + maxItemSize + ")");
             return;
         }
 
-        ItemStack item = e.getItem().getItemStack();
-        try {
-            // Note: We checked if the entity was a player above, so we do not
-            // need to do it here
-            LivingEntity entity = e.getEntity();
-            Player player = (Player) entity;
-
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            BukkitObjectOutputStream data = new BukkitObjectOutputStream(os);
-            data.writeObject(item);
-            data.close();
-
-            int invSize = inventorySize(player.getInventory().getContents());
-            int itemSize = os.toByteArray().length;
-            os.close();
-
-            if (config.getBoolean("debug")) {
-                player.sendMessage(itemSize + " bytes. Inventory: " + invSize + " bytes.");
-                Bukkit.getLogger().info("Player: " + player.getDisplayName() + ", Item:" + itemSize
-                        + " bytes, Inventory: " + invSize + " bytes, Total: " + (invSize + itemSize) + " bytes.");
-            }
-
-            // Check to make sure inventory contents aren't over 36kb
-            if (itemSize + invSize > maxInventorySize) {
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                        "&8&l(&c&l!&8&l) &cPicking up this item (" + (itemSize + invSize) + "/" + maxInventorySize
-                                + ") would overflow your maximum inventory size, thus it will not be picked up."));
-                e.setCancelled(true);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        // Check if the total inventory size is larger than the maximum
+        // inventory size
+        if (itemSize + invSize > maxInventorySize) {
+            player.sendMessage(ChatColor.RED + "You cannot pick up this item, your inventory is to full ("
+                    + (itemSize + invSize) + " > " + maxInventorySize + ")");
+            e.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onBookSave(PlayerEditBookEvent e) {
+        if (disabled)
+            return;
+
+        // TODO: We should stop using depreciated apis
         List<String> pages = e.getNewBookMeta().getPages();
         String text = String.join("", pages);
         final byte[] bytes = text.getBytes(StandardCharsets.UTF_8);
-        if (bytes.length > 8000) {
-            e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    "&8&l(&c&l!&8&l) &cThe contents of the book are over 8KB, so this book will not be saved to prevent potential exploits."));
+
+        if (bytes.length > maxBookSize) {
+            e.getPlayer().sendMessage(ChatColor.RED + "The contents of the book are too large (" + bytes.length + " > "
+                    + maxBookSize + ")");
             e.setCancelled(true);
         }
     }
